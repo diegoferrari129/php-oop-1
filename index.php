bd6a24d63e952025890c2b870a0ad7d8c6be1697
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-09-17 16:30:32 +0200

    [UPD] created function construct and invoked to instance

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
-<?php 
+<?php
 
-    class Movie { 
+    //definisco una classe
+    class Movie {
+
+        //dichiaro gli attributi della classe 
         public $title;
         public $original_title;
         public $genre;
         public $release_date;
         public $running_time;
+
+        //costrutto che viene invocato nella creazione dell'istanza della classe
+        function __construct($title, $original_title, $genre, $release_date, $running_time){
+            $this->title = $title;
+            $this->original_title = $original_title;
+            $this->genre = $genre;
+            $this->release_date = $release_date;
+            $this->running_time = $running_time;
+        }
+
     }
 
-    $Howls_Moving_Castle = new Movie();
+    //creazione dell'istanza della classe
+    $Howls_Moving_Castle = new Movie('Howl\s Moving Castle', 'ハウルの動く城 (Hauru no Ugoku Shiro)', 'Animation', '5 September 2004', '119 minutes');
     
     var_dump($Howls_Moving_Castle);
-
 ?>
 
 <!DOCTYPE html>
