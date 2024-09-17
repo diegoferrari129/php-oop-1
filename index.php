906d787d33c4dad50d519a8bc96c5de19c08f4b1
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-09-17 18:13:32 +0200

    [UPD] added implode to genre array to display multiple genres

--- index.php
+++ index.php
@@ -6,30 +6,34 @@
         //dichiaro gli attributi della classe 
         public $title;
         public $original_title;
-        public $genre;
+        public $genres;
         public $release_date;
         public $running_time;
 
         //costrutto che viene invocato nella creazione dell'istanza della classe
-        function __construct($title, $original_title, $genre, $release_date, $running_time){
+        function __construct($title, $original_title, Array $genres, $release_date, $running_time) {
             $this->title = $title;
             $this->original_title = $original_title;
-            $this->genre = $genre;
+            $this->genres = $genres;
             $this->release_date = $release_date;
             $this->running_time = $running_time;
         }
 
         //metodo per mostrare gli oggetti a schermo
         public function showMovie() {
-            return $this->title.' '.$this->original_title.' '.$this->genre.' '.$this->release_date.' '.$this->running_time;
+            
+            //converto l'array in una stringa
+            $genres_array = implode(', ', $this->genres);
+
+            return $this->title.' '.$this->original_title.' '.$genres_array.' '.$this->release_date.' '.$this->running_time;
         }
 
     }
 
     //creazione delle istanze della classe Movie
-    $movie_1 = new Movie('Howl/s Moving Castle', 'ハウルの動く城 (Hauru no Ugoku Shiro)', 'Animation', '5 September 2004', '119 minutes');
+    $movie_1 = new Movie('Howl/s Moving Castle', 'ハウルの動く城 (Hauru no Ugoku Shiro)', ['Animation', 'Fantasy', 'Adventure'], '5 September 2004', '119 minutes');
 
-    $movie_2  = new Movie('5 Centimeters per Second', '秒速5センチメートル (Byōsoku Go Senchimētoru)', 'Animation', '3 March 2007', '63 minutes');
+    $movie_2  = new Movie('5 Centimeters per Second', '秒速5センチメートル (Byōsoku Go Senchimētoru)', ['Animation', 'Romantic', 'Drama'], '3 March 2007', '63 minutes');
 
     //invoco il metodo showMovie
     echo $movie_1->showMovie();
