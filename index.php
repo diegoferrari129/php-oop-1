32dc0d76c1e4c4af2d5c5d29eac1ffef65d6e283
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-09-17 15:37:24 +0200

    [UPD] created class Movie with attributes

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php 
 
+    class Movie { 
+        public $title;
+        public $original_title;
+        public $genre;
+        public $release_date;
+        public $running_time;
+    }
+
+    $Howls_Moving_Castle = new Movie();
+    
+    var_dump($Howls_Moving_Castle);
+
 ?>
 
 <!DOCTYPE html>
